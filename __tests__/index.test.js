56b1e6c84659f68cd969d494e107f2ca83d5cc19
NormanBenbrahim <norman.benbrahim@protonmail.com>
2021-12-23 13:51:23 -0500

    cleanup

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,152 +2,152 @@ const MyAdminAPI = require('../index')
 const server = require('../mock-server')
 
 const constructorProperties = {
-	apiKey: 'an apiKey',
-	password: 'a password',
-	sessionId: 'a sessionId',
-	uri: 'https://myadminapitest.geotab.com/v2/MyAdminApi.ashx',
-	username: 'a username'
+  apiKey: 'an apiKey',
+  password: 'a password',
+  sessionId: 'a sessionId',
+  uri: 'https://myadminapitest.geotab.com/v2/MyAdminApi.ashx',
+  username: 'a username'
 }
 
 const sut = new MyAdminAPI({
-	...constructorProperties,
-	username: process.env.GEOTAB_USERNAME,
-	password: process.env.GEOTAB_PASSWORD
+  ...constructorProperties,
+  username: process.env.GEOTAB_USERNAME,
+  password: process.env.GEOTAB_PASSWORD
 })
 
 beforeAll(() => {
-	server.listen()
+  server.listen()
 })
 
 afterAll(() => {
-	server.close()
+  server.close()
 })
 
 afterEach(() => {
-	server.resetHandlers()
+  server.resetHandlers()
 })
 
 describe('MyAdminAPI constructor', () => {
-	const instantiateClass = (obj) => {
-		return () => {
-			return new MyAdminAPI(obj)
-		}
-	}
-	it('requires an object', () => {
-		expect(instantiateClass()).toThrow('Cannot')
-		expect(instantiateClass({})).toThrow('Must supply')
-	})
-	it('requires an apiKey', () => {
-		expect(instantiateClass({
-			...constructorProperties,
-			apiKey: null,
-		})).toThrow('Must supply apiKey.')
-	})
-	it('requires a password', () => {
-		expect(instantiateClass({
-			...constructorProperties,
-			password: null,
-		})).toThrow('Must supply password.')
-	})
-	it('requires a sessionId', () => {
-		expect(instantiateClass({
-			...constructorProperties,
-			sessionId: null,
-		})).toThrow('Must supply sessionId.')
-	})
-	it('requires a username', () => {
-		expect(instantiateClass({
-			...constructorProperties,
-			username: null
-		})).toThrow('Must supply username.')
-	})
-	it('uses production url by default', () => {
-		expect(new MyAdminAPI({
-			...constructorProperties,
-			uri: null
-		}).serverUrl).toEqual('https://myadminapi.geotab.com/v2/MyAdminApi.ashx')
-	})
-	it('uses given url', () => {
-		expect(new MyAdminAPI({
-			...constructorProperties,
-			uri: `any url`
-		}).serverUrl).toEqual('any url')
-	})
-	it('uses given credentials', () => {
-		expect(new MyAdminAPI(constructorProperties).credentials).toEqual({
-			apiKey: 'an apiKey',
-			password: 'a password',
-			sessionId: 'a sessionId',
-			username: 'a username'
-		})
-	})
+  const instantiateClass = (obj) => {
+    return () => {
+      return new MyAdminAPI(obj)
+    }
+  }
+  it('requires an object', () => {
+    expect(instantiateClass()).toThrow('Cannot')
+    expect(instantiateClass({})).toThrow('Must supply')
+  })
+  it('requires an apiKey', () => {
+    expect(instantiateClass({
+      ...constructorProperties,
+      apiKey: null,
+    })).toThrow('Must supply apiKey.')
+  })
+  it('requires a password', () => {
+    expect(instantiateClass({
+      ...constructorProperties,
+      password: null,
+    })).toThrow('Must supply password.')
+  })
+  it('requires a sessionId', () => {
+    expect(instantiateClass({
+      ...constructorProperties,
+      sessionId: null,
+    })).toThrow('Must supply sessionId.')
+  })
+  it('requires a username', () => {
+    expect(instantiateClass({
+      ...constructorProperties,
+      username: null
+    })).toThrow('Must supply username.')
+  })
+  it('uses production url by default', () => {
+    expect(new MyAdminAPI({
+      ...constructorProperties,
+      uri: null
+    }).serverUrl).toEqual('https://myadminapi.geotab.com/v2/MyAdminApi.ashx')
+  })
+  it('uses given url', () => {
+    expect(new MyAdminAPI({
+      ...constructorProperties,
+      uri: `any url`
+    }).serverUrl).toEqual('any url')
+  })
+  it('uses given credentials', () => {
+    expect(new MyAdminAPI(constructorProperties).credentials).toEqual({
+      apiKey: 'an apiKey',
+      password: 'a password',
+      sessionId: 'a sessionId',
+      username: 'a username'
+    })
+  })
 })
 
 describe('MyAdminAPI.authenticate()', () => {
-	it('is a function', () => {
-		expect(typeof sut.authenticate).toBe('function')
-	})
-	it('returns an object and sets the credentials', async () => {
-		const authenticatedSUT = await sut.authenticate()
-		expect(typeof authenticatedSUT).toBe('object')
-		expect(sut.credentials).not.toEqual({
-			...constructorProperties,
-			username: process.env.GEOTAB_USERNAME,
-			password: process.env.GEOTAB_PASSWORD
-		})
-		expect(sut.credentials.apiKey).not.toEqual('an apiKey')
-		expect(sut.credentials.sessionId).not.toEqual('a session id')
-	})
-	it.skip('fails', () => {})
+ it('is a function', () => {
+    expect(typeof sut.authenticate).toBe('function')
+  })
+  it('returns an object and sets the credentials', async () => {
+    const authenticatedSUT = await sut.authenticate()
+    expect(typeof authenticatedSUT).toBe('object')
+    expect(sut.credentials).not.toEqual({
+      ...constructorProperties,
+      username: process.env.GEOTAB_USERNAME,
+      password: process.env.GEOTAB_PASSWORD
+    })
+    expect(sut.credentials.apiKey).not.toEqual('an apiKey')
+    expect(sut.credentials.sessionId).not.toEqual('a session id')
+  })
+  it.skip('fails', () => {})
 })
 
 describe('MyAdminAPI.call()', () => {
-	it('throws error when not provided a method name', async () => {
-		await sut.authenticate()
-		expect(sut.call(null, null)).rejects.toThrow('Must provide method.')
-	})
-	it('fetches "GetCountries" (a supported method without params)', async () => {
-		await sut.authenticate()
-		expect(await sut.call('GetCountries', null)).toEqual(
-			expect.arrayContaining(['Canada', 'United States'])
-			)
-	})
-	it('fetches "GetDevicePlans" (a supported method with params)', async () => {
-		await sut.authenticate()
-		expect(await sut.call('GetStates', { countryName: 'Canada' })).toEqual(
-			expect.arrayContaining(['Alberta', 'British Columbia'])
-		)
-	})
-	it.skip('fails', () => {})
+  it('throws error when not provided a method name', async () => {
+    await sut.authenticate()
+    expect(sut.call(null, null)).rejects.toThrow('Must provide method.')
+  })
+  it('fetches "GetCountries" (a supported method without params)', async () => {
+    await sut.authenticate()
+    expect(await sut.call('GetCountries', null)).toEqual(
+      expect.arrayContaining(['Canada', 'United States'])
+      )
+  })
+  it('fetches "GetDevicePlans" (a supported method with params)', async () => {
+    await sut.authenticate()
+    expect(await sut.call('GetStates', { countryName: 'Canada' })).toEqual(
+      expect.arrayContaining(['Alberta', 'British Columbia'])
+    )
+  })
+  it.skip('fails', () => {})
 })
 
 describe('MyAdminAPI.post(), can also be called directly (with credentials)', () => {
-	it('returns error message if missing method name (does not throw)', async () => {
-		await sut.authenticate()
-		const result = await sut.post('', {
-			params: null,
-			apiKey: sut.credentials.apiKey,
-			sessionId: sut.credentials.sessionId,
-    })
-		expect(result.error.code).toBeUndefined()
-		expect(result.error.name).toEqual('MissingMethodException')
-		expect(result.error.message.indexOf('An error has occurred')).not.toEqual(-1)
-	})
-	it('fetches "GetCountries" (a supported method without params)', async () => {
-		await sut.authenticate()
-		expect(await sut.post('GetCountries', {
+  it('returns error message if missing method name (does not throw)', async () => {
+    await sut.authenticate()
+    const result = await sut.post('', {
       params: null,
       apiKey: sut.credentials.apiKey,
       sessionId: sut.credentials.sessionId,
-    })).toEqual(expect.arrayContaining(['Canada', 'United States']))
-	})
+  })
+    expect(result.error.code).toBeUndefined()
+    expect(result.error.name).toEqual('MissingMethodException')
+    expect(result.error.message.indexOf('An error has occurred')).not.toEqual(-1)
+  })
+  it('fetches "GetCountries" (a supported method without params)', async () => {
+    await sut.authenticate()
+    expect(await sut.post('GetCountries', {
+   params: null,
+   apiKey: sut.credentials.apiKey,
+   sessionId: sut.credentials.sessionId,
+  })).toEqual(expect.arrayContaining(['Canada', 'United States']))
+  })
 })
 
 describe('MyAdminAPI still supports old method names', () => {
-	it('authenticateAsync() is still a usable function name', () => {
-		expect(typeof sut.authenticateAsync).toBe('function')
-	})
-	it('callAsync() is still a usable function name', () => {
-		expect(typeof sut.callAsync).toBe('function')
-	})
+  it('authenticateAsync() is still a usable function name', () => {
+    expect(typeof sut.authenticateAsync).toBe('function')
+  })
+  it('callAsync() is still a usable function name', () => {
+   expect(typeof sut.callAsync).toBe('function')
+  })
 })
--- mock-server.js
+++ mock-server.js
@@ -3,55 +3,55 @@ const rest = require('msw').rest
 const setupServer = require('msw/node').setupServer
 
 const server = setupServer(
-	// calls that do not require parameter inputs
-	rest.post(process.env.GEOTAB_SANDBOX_URL, (req, res, ctx) => {
-		switch(req.body.method) {
-			case 'GetCountries': {
-				return res(
-					ctx.status(200),
-					ctx.json({
-						result: ['Canada', 'Philippines', 'United States']
-						})
-					)
-				
-				}
-			case 'GetStates': {
-				return res(
-					ctx.status(200),
-					ctx.json({
-						result: ['Alberta','any other province','British Columbia']
-					})
-				)
-			}
-			case '': {
-				return res(
-				ctx.status(500),
-				ctx.json({
-					result: {
-						error: {
-							message: 'An error has occurred and this string contains info.',
-							name: 'MissingMethodException'
-						}
-					}
-				})
-				)
-			}
-			// return a default so our tests don't error out
-			default: {
-				return res(
-					ctx.status(200), 
-					ctx.json({
-						result: {
-							among_other_things: 'this result contains sessionId and userId, below',
-							sessionId: 'a session Id',
-							userId: 'a userId',
-						}
-					})
-				)		
-			}
-		}
-	})
-	// call for request
+  // calls that do not require parameter inputs
+  rest.post(process.env.GEOTAB_SANDBOX_URL, (req, res, ctx) => {
+    switch(req.body.method) {
+      case 'GetCountries': {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            result: ['Canada', 'Philippines', 'United States']
+            })
+          )
+        
+        }
+      case 'GetStates': {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            result: ['Alberta','any other province','British Columbia']
+          })
+        )
+      }
+      case '': {
+        return res(
+        ctx.status(500),
+        ctx.json({
+          result: {
+            error: {
+              message: 'An error has occurred and this string contains info.',
+              name: 'MissingMethodException'
+            }
+          }
+        })
+        )
+      }
+      // return a default so our tests don't error out
+      default: {
+        return res(
+          ctx.status(200), 
+          ctx.json({
+            result: {
+              among_other_things: 'this result contains sessionId and userId, below',
+              sessionId: 'a session Id',
+              userId: 'a userId',
+            }
+          })
+        )		
+      }
+    }
+  })
+  // call for request
 )
 
 module.exports = server
